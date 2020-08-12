be0d2372d66f24a1a2cf9ed6be78c97086e4d93c
Kevin Goodwin <fwmechanic@gmail.com>
2020-08-12 13:43:06 -0700

    update comments as this script is as obscure as it gets...

--- gb.bat
+++ gb.bat
@@ -3,15 +3,18 @@ setlocal
 
 REM my installation of Git for Windows is (for reasons which I now forget)
 REM installed in %LOCALAPPDATA%\programs\Git rather than the "standard" location
-REM which existing answers https://stackoverflow.com/a/17306604 assume
-REM since I want a tool that will work for either case, this hackery ensues...
+REM which existing answers https://stackoverflow.com/a/17306604 assume.
+REM since I want a tool that will work for either case, AND because Git for
+REM Windows CAN BE installed with bash and other unix utils it includes
+REM OMITTED FROM PATH, this hackery ensues...
+REM ... It finds bash.exe relative to git.exe
 REM in MY standard git install config, sh.exe is not in PATH, but git.exe is...
 REM 20171007 kgoodwin
 
 call :GetAbsPathOfExe "git.exe"
 if "%filename%" == "" (
   echo git.exe was not found in PATH?!
-  exit /b
+  exit 66 /b
 )
 call :RmvOnePathEl "%filename%"
 call :RmvOnePathEl "%filename%"
@@ -21,21 +24,23 @@ if exist "%filename%" (
   start "" "%filename%" --login
 ) else (
   echo weird Git For Windows install; sh.exe not found relative to git.exe path
+  exit 66 /b
 )
-exit /b
 
 ::----------------------------------
 
 REM Windows BATch language sucks SO BAD!!!
-REM Certain magical variable substitutions are ONLY available
-REM to PARAMETER variables, not to NAMED variables,
-REM so we create subroutines which accept PARAMETER variables
-REM and operate on a GLOBAL (!!!) NAMED variable filename.
+REM Certain magical variable substitutions
+REM are ONLY available to PARAMETER variables, NOT to NAMED variables,
+REM so we create artificial subroutines which exist solely to accept PARAMETER variables
+REM and perform PARAMETER variable substitutions on them
+REM to obtain values to assign to the GLOBAL NAMED variable "filename".
 REM Better code may be possible, but I'm not going to worry
 REM as long as it works reliably.
 
 :GetAbsPathOfExe
    REM echo %1
+  REM use the internal equivalent of where.exe; faster?
   set "filename=%~$PATH:1"
    REM echo %filename%
   exit /b
